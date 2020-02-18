756296af40bfef7cae18c420bd1e2cbd08b9ade1
unknown <shidalgo1212@gmail.com>
2020-02-17 22:51:17 -0600

    Resource tables modified

---
+++ EDU.sql
@@ -0,0 +1,18 @@
+-- drop table edu
+CREATE TABLE edu (
+	country VARCHAR,
+	c_codes VARCHAR,
+	cal_year INT,
+	GNP DECIMAL,
+	Population BIGINT,
+	Edu_prime DECIMAL
+);
+
+-- drop table HDI
+CREATE TABLE HDI(
+	Country VARCHAR,
+	HDI_Rank_2018 INT,
+	cal_year INT,
+	HDI DECIMAL
+);
+
